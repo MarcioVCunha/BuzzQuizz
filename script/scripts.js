a89453265de9ad721dbac2b9bf5e96b571e82274
Marcio <marcioabcd11@gmail.com>
2021-08-11 11:16:29 -0300

    Segond Page In Progress

--- script/scripts.js
+++ script/scripts.js
@@ -1,8 +1,30 @@
 const mainScreen = document.querySelector('main');
+const nameQuizzScreen = document.querySelector('.name-quizz-screen');
+const questionQuizzScreen = document.querySelector('.create-question-screen');
 
 function showCreateQuizzScreen(){
-    let nameQuizzScreen = document.querySelector(".name-Quizz-Screen");
-    
     mainScreen.classList.toggle('hidden');
     nameQuizzScreen.classList.toggle('hidden');
+}
+
+function showCreateQuestionsScreen(){
+    nameQuizzScreen.classList.toggle('hidden');
+    createQuestionsBoxes();
+    questionQuizzScreen.classList.toggle('hidden');
+}
+
+function createQuestionsBoxes(){
+    let numberOfQuestions = Number(nameQuizzScreen.querySelector('.number-of-questions').value);
+
+    questionQuizzScreen.querySelector('ul').innerHTML = '';
+
+    for(let i = 0; i < numberOfQuestions; i++){
+        questionQuizzScreen.querySelector('ul').innerHTML += `
+        <li>
+            <div class="conteiner-questions">
+                <p>Pergunta ${i+1}</p>
+                <ion-icon name="create-outline"></ion-icon>
+            </div>
+        </li>`;
+    }
 }
